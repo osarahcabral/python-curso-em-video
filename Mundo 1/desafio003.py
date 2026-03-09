10a96bbf5d2b8d73820a77b30657d0c3d4aa896e
Sarah Cabral <osarahcabral@gmail.com>
2026-03-09 12:36:21 -0300

    Add desafio003

---
+++ Mundo 1/desafio003.py
@@ -0,0 +1,6 @@
+# Desafio 003 
+# Crie um programa que leia dois números e mostre a soma entre eles.
+
+num1 = int(input('Digite o primeiro número:'))
+num2 = int(input('Digite o segundo número:'))
+print("A soma entre {} e {} é: {}".format(num1,num2,num1+num2))
