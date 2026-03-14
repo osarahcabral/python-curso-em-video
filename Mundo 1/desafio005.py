f07bf47cf834ad4f59bcb462804edf9092730a1b
Sarah Cabral <osarahcabral@gmail.com>
2026-03-14 15:40:55 -0300

    Add desafio005
    Implement a program to display the predecessor and successor of an integer input.

---
+++ Mundo 1/desafio005.py
@@ -0,0 +1,5 @@
+#Desafio 005
+#Faça um programa que leia um número inteira e mostra na tela o seu sucessor e seu antecessor
+
+n = int(input('Digite um valor: '))
+print('Seu antecessor é {} e o seu sucessor é {}'.format(n-1,n+1))
